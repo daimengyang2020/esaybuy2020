645ab040aed3583c43d490955b86670b5c2fa610
daimengyang <2283753684@qq.com>
2020-02-20 10:57:49 +0800

    易买网搭建框架 使用easycode自动生成实体类 查询一级菜单

--- src/com/buy/dao/product/IProductCategory.java
+++ src/com/buy/dao/product/IProductCategory.java
@@ -5,5 +5,6 @@ import com.buy.entity.EasybuyProductCategory;
 import java.util.List;
 
 public interface IProductCategory {
+    //查询一级菜单
     List<EasybuyProductCategory> queryAllProductCategory(String parentId);
 }
--- src/com/buy/dao/product/ProductCategoryImpl.java
+++ src/com/buy/dao/product/ProductCategoryImpl.java
@@ -3,39 +3,64 @@ package com.buy.dao.product;
 import com.buy.entity.EasybuyProductCategory;
 import com.buy.utils.DataSourceUtil;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductCategoryImpl implements IProductCategory{
+/**
+ * @Author:
+ * @Date: 2020/2/19 10:00
+ * @Description:
+ */
+public class ProductCategoryImpl implements IProductCategory {
+
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+
     @Override
     public List<EasybuyProductCategory> queryAllProductCategory(String parentId) {
-        List<EasybuyProductCategory> productCategories=new ArrayList<EasybuyProductCategory>();
-        EasybuyProductCategory productCategory=null;
+        List<EasybuyProductCategory> productCategories = new ArrayList<EasybuyProductCategory>();
+        EasybuyProductCategory productCategory = null;
+
         try {
-            StringBuffer sql=new StringBuffer();
+            StringBuffer sql = new StringBuffer();
             sql.append("select * from easybuy_product_category where 1=1");
-            if(!"".equals(parentId)||null!=parentId){
-              sql.append(" and parentId = 0");
+
+            //判断parentID的值，如果为0，显示的是一级分类
+            if (!"".equals(parentId) && null != parentId) {
+                sql.append(" and parentId = ?");
             }
-            Connection conn= DataSourceUtil.getconn();
-            PreparedStatement pstmt=conn.prepareStatement(sql.toString());
-            ResultSet rs=pstmt.executeQuery();
-            while (rs.next()){
-                productCategory=new EasybuyProductCategory();
+
+            //获取连接
+            conn = DataSourceUtil.getconn();
+            pstmt = conn.prepareStatement(sql.toString());
+            if (!"".equals(parentId) && null != parentId) {
+                pstmt.setObject(1, parentId);
+            }
+
+            rs = pstmt.executeQuery();
+
+            //处理结果集
+            while (rs.next()) {
+                //实例化对象
+                productCategory = new EasybuyProductCategory();
                 productCategory.setId(rs.getInt(1));
                 productCategory.setName(rs.getString(2));
                 productCategory.setParentid(rs.getInt(3));
                 productCategory.setType(rs.getInt(4));
                 productCategory.setIconclass(rs.getString(5));
+                //将对象填充到集合中
                 productCategories.add(productCategory);
             }
-        }catch (Exception e){
+            System.out.println(sql.toString());
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
         return productCategories;
     }
 }
