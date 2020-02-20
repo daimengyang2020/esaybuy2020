1a08b208fe5e7a9484ebffbb841790eff669f596
daimengyang <2283753684@qq.com>
2020-02-20 11:15:24 +0800

    1编写获取商品分类信息的service层 2实现输出

--- src/com/buy/test/demo1.java
+++ src/com/buy/test/demo1.java
@@ -4,13 +4,15 @@ package com.buy.test;
 import com.buy.dao.product.IProductCategory;
 import com.buy.dao.product.ProductCategoryImpl;
 import com.buy.entity.EasybuyProductCategory;
+import com.buy.service.product.IproductCategoryService;
+import com.buy.service.product.ProductCategoryServiceimpl;
 
 import java.util.List;
 
 public class demo1 {
     public static void main(String[] args) {
-        IProductCategory productCategorg=new ProductCategoryImpl();
-        List<EasybuyProductCategory> productCategories=productCategorg.queryAllProductCategory("0");
+        IproductCategoryService service=new ProductCategoryServiceimpl();
+        List<EasybuyProductCategory> productCategories=service.queryAllProductCategory("0");
         for (EasybuyProductCategory category:productCategories){
             System.out.println(category.getName());
         }
